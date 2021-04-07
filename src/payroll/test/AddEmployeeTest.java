abac1f950d84f89928f356f1bbf01f39b03502b9
kagKuSqu <shanyi614dvo@126.com>
2021-04-07 21:34:02 +0800

    新建测试

---
+++ src/payroll/test/AddEmployeeTest.java
@@ -0,0 +1,14 @@
+package payroll.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AddEmployeeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
