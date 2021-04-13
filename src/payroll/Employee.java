c580476c529d45dbc0da9c0a26a2a8d172c71ca5
kagKuSqu <shanyi614dvo@126.com>
2021-04-13 18:19:57 +0800

    生成getters和setters功能

--- src/payroll/Employee.java
+++ src/payroll/Employee.java
@@ -4,4 +4,23 @@ public class Employee {
 	private int emplId;
 	private String name;
 	private String address;
+	public int getEmplId() {
+		return emplId;
+	}
+	public void setEmplId(int emplId) {
+		this.emplId = emplId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
 }
