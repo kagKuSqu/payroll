c557e5fd8795ded5c40a35cecc46b65b5d09944c
kagKuSqu <shanyi614dvo@126.com>
2021-04-13 18:08:23 +0800

    其他也赋值新字段

--- src/payroll/trans/AddHourlyEmployeeTransation.java
+++ src/payroll/trans/AddHourlyEmployeeTransation.java
@@ -5,10 +5,15 @@ import payroll.Transaction;
 public class AddHourlyEmployeeTransation implements Transaction {
 
 	private int empId;
+	private String address;
+	private double hourlyRate;
+	private String name;
 
 	public AddHourlyEmployeeTransation(int empId, String name, String address, double hourlyRate) {
 		this.empId = empId;
-		// TODO Auto-generated constructor stub
+		this.name = name;
+		this.address = address;
+		this.hourlyRate = hourlyRate;
 	}
 
 }
