1403b426b7d7ef03dcee4e96cd140503a154a01b
kagKuSqu <shanyi614dvo@126.com>
2021-04-13 19:04:17 +0800

    模拟数据库存储

---
+++ src/payroll/PayrollDatabase.java
@@ -0,0 +1,14 @@
+package payroll;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PayrollDatabase {
+
+	private static Map<Integer, Employee> employees=new HashMap<Integer,Employee>(); {
+		public static Employee getEmployee(int empId) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+}
