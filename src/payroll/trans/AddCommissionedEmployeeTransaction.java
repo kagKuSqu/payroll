8af3116a729202cc7d9154262ba1d05495d22074
kagKuSqu <shanyi614dvo@126.com>
2021-04-20 17:59:53 +0800

    添加够着方法

--- src/payroll/trans/AddCommissionedEmployeeTransaction.java
+++ src/payroll/trans/AddCommissionedEmployeeTransaction.java
@@ -1,21 +1,24 @@
 package payroll.trans;
 
 import payroll.PaymentClassification;
+import payroll.classification.CommissionedClassification;
 
 public class AddCommissionedEmployeeTransaction extends AddEmployeeTransaction {
 
 	private double salary;
+	private double commissionRate;
 
 	public AddCommissionedEmployeeTransaction(int empId, String name, String address, double salary,
 			double commissionRate) {
 		    super(empId,name,address);
 			this.salary = salary;
+			this.commissionRate=commissionRate;
 	}
 
 	@Override
 	protected PaymentClassification getPaymentClassification() {
 		// TODO Auto-generated method stub
-		return null;
+		return new CommissionedClassification();
 	}
 
 }
