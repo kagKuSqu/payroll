8bcf733e14df896fba1bec68c0f17b2cdf75439f
kagKuSqu <shanyi614dvo@126.com>
2021-04-20 15:22:31 +0800

    赋值给字段

--- src/payroll/classification/SalariedClassification.java
+++ src/payroll/classification/SalariedClassification.java
@@ -3,14 +3,12 @@ package payroll.classification;
 import payroll.PaymentClassification;
 
 public class SalariedClassification extends PaymentClassification {
-
+	private double salary;
 	public SalariedClassification(double salary) {
-		// TODO Auto-generated constructor stub
+		this.salary = salary;
+		
 	}
-
 	public double getSalary() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
-
 }
