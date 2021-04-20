bb305d99f0ad1f03127a70edca950296c81a6cb4
kagKuSqu <shanyi614dvo@126.com>
2021-04-20 15:53:38 +0800

    AddSalariedEmployeeTransaction调用超类构造方法

--- src/payroll/trans/AddSalariedEmployeeTransaction.java
+++ src/payroll/trans/AddSalariedEmployeeTransaction.java
@@ -7,9 +7,7 @@ import payroll.classification.SalariedClassification;
 public class AddSalariedEmployeeTransaction extends AddEmployeeTransaction implements Transaction {
 	private double salary;
 	public AddSalariedEmployeeTransaction(int empId, String name, String address, double salary) {
-		this.empId=empId;
-		this.name=name;
-		this.address=address;
+		super(empId,name,address);
 		this.salary=salary;
 	}
 
