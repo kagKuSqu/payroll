a53acbe751cf4135a63642e6d8e0901debdf4233
kagKuSqu <shanyi614dvo@126.com>
2021-04-20 18:40:24 +0800

    添加NoSuchEmployeeException构造方法

--- src/payroll/exception/NoSuchEmployeeException.java
+++ src/payroll/exception/NoSuchEmployeeException.java
@@ -2,4 +2,13 @@ package payroll.exception;
 
 public class NoSuchEmployeeException extends RuntimeException {
 
+	public NoSuchEmployeeException(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 }
