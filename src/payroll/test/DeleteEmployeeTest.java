7963fb316c68f0994dd1f8d2efdb63e8b46e7818
kagKuSqu <shanyi614dvo@126.com>
2021-04-20 18:27:43 +0800

    删除雇员

--- src/payroll/test/DeleteEmployeeTest.java
+++ src/payroll/test/DeleteEmployeeTest.java
@@ -4,13 +4,17 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import payroll.Transaction;
+import payroll.trans.AddHourlyEmployeeTransation;
+
 public class DeleteEmployeeTest {
 
 	@Test
 	public void testDeleteEmployeeExisted() {
 		int empId=2001;
-		new AddHourlyEmployeeTransaction(empId,"Bill",12.5).execute();
-		
+		new AddHourlyEmployeeTransation(empId,"Bill","Home",12.5).execute();
+		Transaction t=new DeleteEmployeeTransaction(empId);
+		t.execute();
 	}
 
 }
