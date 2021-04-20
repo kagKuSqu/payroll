5cfc828ba28dd5888ba7653ae5ef053735d50559
kagKuSqu <shanyi614dvo@126.com>
2021-04-20 17:51:01 +0800

    赋值字段

--- src/payroll/trans/AddCommissionedEmployeeTransaction.java
+++ src/payroll/trans/AddCommissionedEmployeeTransaction.java
@@ -4,9 +4,12 @@ import payroll.PaymentClassification;
 
 public class AddCommissionedEmployeeTransaction extends AddEmployeeTransaction {
 
+	private double salary;
+
 	public AddCommissionedEmployeeTransaction(int empId, String name, String address, double salary,
 			double commissionRate) {
 		    super(empId,name,address);
+			this.salary = salary;
 	}
 
 	@Override
