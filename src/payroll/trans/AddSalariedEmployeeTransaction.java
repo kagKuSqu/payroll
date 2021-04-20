d8c1bd622621fc91d2791453aa5251f2c318a560
kagKuSqu <shanyi614dvo@126.com>
2021-04-20 15:32:25 +0800

    替换为超类PaymentClassification

--- src/payroll/trans/AddSalariedEmployeeTransaction.java
+++ src/payroll/trans/AddSalariedEmployeeTransaction.java
@@ -1,6 +1,7 @@
 package payroll.trans;
 
 import payroll.Employee;
+import payroll.PaymentClassification;
 import payroll.PayrollDatabase;
 import payroll.Transaction;
 import payroll.classification.SalariedClassification;
@@ -27,7 +28,7 @@ public class AddSalariedEmployeeTransaction implements Transaction {
 
 	}
 
-	protected SalariedClassification getPaymentClassification() {
+	protected PaymentClassification getPaymentClassification() {
 		return new SalariedClassification(salary);
 	}
 
