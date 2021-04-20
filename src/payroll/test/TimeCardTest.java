54c33ec993c79ae831899c0ac2b57aaab99a1144
kagKuSqu <shanyi614dvo@126.com>
2021-04-20 18:47:13 +0800

    建立一个TimeCardTest类

---
+++ src/payroll/test/TimeCardTest.java
@@ -0,0 +1,14 @@
+package payroll.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TimeCardTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
