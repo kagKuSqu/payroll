a851fc0390271e8333191197a9a4904c032409e9
kagKuSqu <shanyi614dvo@126.com>
2021-04-20 15:05:04 +0800

    添加getSalary方法

--- src/payroll/classification/SalariedClassification.java
+++ src/payroll/classification/SalariedClassification.java
@@ -4,4 +4,9 @@ import payroll.PaymentClassification;
 
 public class SalariedClassification extends PaymentClassification {
 
+	public double getSalary() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
