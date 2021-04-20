dba61504feafc91a252b665125cea60e986b0fad
kagKuSqu <shanyi614dvo@126.com>
2021-04-20 18:30:21 +0800

    添加DeleteEmployeeTransaction构造方法

--- src/payroll/trans/DeleteEmployeeTransaction.java
+++ src/payroll/trans/DeleteEmployeeTransaction.java
@@ -4,6 +4,10 @@ import payroll.Transaction;
 
 public class DeleteEmployeeTransaction implements Transaction {
 
+	public DeleteEmployeeTransaction(int empId) {
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public void execute() {
 		// TODO Auto-generated method stub
