de8032740b139fcb6f40178d4f3e9de484ce97ad
kagKuSqu <shanyi614dvo@126.com>
2021-04-20 15:02:14 +0800

    添加AddSalariedEmployeeTransaction构造函数

--- src/payroll/trans/AddSalariedEmployeeTransaction.java
+++ src/payroll/trans/AddSalariedEmployeeTransaction.java
@@ -4,6 +4,10 @@ import payroll.Transaction;
 
 public class AddSalariedEmployeeTransaction implements Transaction {
 
+	public AddSalariedEmployeeTransaction(int empId, String name, String address, double salary) {
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public void excute() {
 		// TODO Auto-generated method stub
