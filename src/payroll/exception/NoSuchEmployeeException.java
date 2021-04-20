d30e1a2cc7e6d32dba3107ba9af5b30093621f15
kagKuSqu <shanyi614dvo@126.com>
2021-04-20 18:43:22 +0800

    测试通过

--- src/payroll/exception/NoSuchEmployeeException.java
+++ src/payroll/exception/NoSuchEmployeeException.java
@@ -2,13 +2,9 @@ package payroll.exception;
 
 public class NoSuchEmployeeException extends RuntimeException {
 
-	public NoSuchEmployeeException(String string) {
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+	public NoSuchEmployeeException(String message) {
+		super(message);
+	}
 
 }
