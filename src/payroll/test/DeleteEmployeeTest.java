d287cedd7d7e9f998fbb3906e4fd04deb051dab8
kagKuSqu <shanyi614dvo@126.com>
2021-04-20 18:17:34 +0800

    建立DeleteEmployeeTest类

---
+++ src/payroll/test/DeleteEmployeeTest.java
@@ -0,0 +1,14 @@
+package payroll.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DeleteEmployeeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
