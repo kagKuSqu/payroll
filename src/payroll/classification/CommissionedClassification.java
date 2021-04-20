72cdf56f8723134acc2d2732410a9ae61c509ae7
kagKuSqu <shanyi614dvo@126.com>
2021-04-20 18:07:01 +0800

    赋值并实现方法

--- src/payroll/classification/CommissionedClassification.java
+++ src/payroll/classification/CommissionedClassification.java
@@ -3,15 +3,23 @@ package payroll.classification;
 import payroll.PaymentClassification;
 
 public class CommissionedClassification extends PaymentClassification {
+	private double salary;
+	private double commissionRate;
+
+	public CommissionedClassification(double salary,double commissionRate) {
+		this.salary = salary;
+		this.commissionRate = commissionRate;
+		
+	}
 
 	public double getSalary() {
 		// TODO Auto-generated method stub
-		return 0;
+		return salary;
 	}
 
 	public double getCommissionRate() {
 		// TODO Auto-generated method stub
-		return 0;
+		return commissionRate;
 	}
 
 }
